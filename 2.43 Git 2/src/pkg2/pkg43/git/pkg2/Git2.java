343368de392ab236d5544977984e21056528f183
Elvin Bayramov <bayramov.elvin@yahoo.com>
2022-05-07 23:08:42 +0400

    BY ELVIN

---
+++ 2.43 Git 2/src/pkg2/pkg43/git/pkg2/Git2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2.pkg43.git.pkg2;
+
+/**
+ *
+ * @author HP
+ */
+//   1. front end proyekti ucun bir dene tapwiriq yaradiriq issue bolmesinde 
+//   2. title bolmesinde bawliq yaziram. task vermek ucun misalcun bele yaziriq : TASK-1: layoutun hazirlanmasi
+//   3. awagisinda izahat verirem lazimdirsa link ile wekil creen filan atiram
+//   4. sagda assignees bolmesinde tapwirigi kime teyin edirem onu secirem. yeni hemin adam iwleyeck
+//   5. project bolmesinde iwlenilen projectin adini secirem. sonra submit edirem.
+//   6. code bolmesinde release2019 branch i yaradiram daha sonra ise releaseden TASK-1 brancini. release bir proyekt 
+// kimi duwunmek olar yeni bu proyekt 2019 a qeder bitmelidir master ise yekun olan branchdir producriona verilen branch
+//   7. her bir tapwiriga bir branch yaradilir
+//   8. masterden release 1 ve release 2 hazirlandi release 1 den ise task1 ve task 2 branchlari yaradilir
+
+public class Git2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
